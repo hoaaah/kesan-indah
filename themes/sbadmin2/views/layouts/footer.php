fccfcfbee12f7816afa1bff7a549310820c5bfc7
Heru Arief Wijaya <hoaaah.arief@gmail.com>
2019-11-14 00:42:43 +0700

    App Title in footer

--- themes/sbadmin2/views/layouts/footer.php
+++ themes/sbadmin2/views/layouts/footer.php
@@ -8,7 +8,7 @@ use kartik\helpers\Html;
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
     <div class="copyright text-center my-auto">
-        <span>Copyright &copy; 2019 - <?= Html::a('<b>hoaaah</b>', 'https://belajararief.com') ?><?= Html::a('@bpkp-aceh-apd2', 'http://bpkp.go.id/aceh.bpkp') ?></span>
+        <span>Kesan Indah v1.0.0 - Copyright &copy; 2019 - <?= Html::a('<b>hoaaah</b>', 'https://belajararief.com') ?><?= Html::a('@bpkp-aceh-apd2', 'http://bpkp.go.id/aceh.bpkp') ?></span>
     </div>
     </div>
 </footer>
